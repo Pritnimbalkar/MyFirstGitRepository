5ba8aea1cb88531d7d9ca36a30e272ab9a92eba5
Pritnimbalkar <pritnimbalkar21112004@gmail.com>
2024-02-03 13:09:06 +0530

    Sum functionality added

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ def fact(n):
     else:
         return n*fact(n-1)
 print(f"The factorial of enterred number is {fact(n)}")
+
+#This is simple python code for adding numbers from 1 to entered number by user
+u=int(input("Enter the number of your choice: "))
+sum=0
+for i in range(1,u+1):
+    sum=i+sum
+print(f"The sum of numbers from one to entered number is {sum}")
+
